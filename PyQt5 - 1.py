2851b191755acd045b2fdcda2d042bdd35c7117b
PornnapusNew <pornnapus.n@gmail.com>
2020-11-24 17:41:19 +0700

    2

--- PyQt5 - 1.py
+++ PyQt5 - 1.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets, uic
 
-class Ui (QtWidgets, QMainWindow):
+class Ui (QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('mainui.ui',self)
