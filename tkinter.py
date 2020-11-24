b1ede32c5ade7ec5907ce7b7fa557623831da4f9
PornnapusNew <pornnapus.n@gmail.com>
2020-11-24 17:39:26 +0700

    1

---
+++ tkinter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter.messagebox
+
+class Calories:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("CALORIES CALCULATOR")
+        self.root.geometry("1350x800")
+        self.root.configure(background = 'Yellow')
+
+#############MainFrame################
+
