f80d17c8e965760e9fc2af9fe73ffd158837c7d7
PornnapusNew <pornnapus.n@gmail.com>
2020-11-24 22:49:13 +0700

    2

--- PyQt5 - 1.py
+++ PyQt5 - 1.py
@@ -5,3 +5,4 @@ class Ui (QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('In class 1.ui',self)
+        self.ok_btn = self.findChild(QtWidgets.QPushButton,'ok_btn') #self.findChildเป็นฟังก์ชันที่ใช้หาargumentใน() ส่วนok_btnคือชื่อ button ที่ตั้งไว้ในPyQt
